6e885f8dfa2c21e2c7aa9bf0e3f765ba954fb107
pushpender673 <pushpender673@gmail.com>
2018-10-02 18:58:52 +0530

    New Added Tab Menu.

---
+++ assets/custom.js
@@ -0,0 +1,34 @@
+var countDownTimer = function(){
+			
+			// Set the date we're counting down to
+			var countDownDate = new Date("Jan 1, 2019 00:00:01").getTime();
+
+			// Update the count down every 1 second
+			var x = setInterval(function() {
+
+			  // Get todays date and time
+			  var now = new Date().getTime();
+
+			  // Find the distance between now and the count down date
+			  var distance = countDownDate - now;
+
+			  // Time calculations for days, hours, minutes and seconds
+			  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+			  // Display the result in the element
+			  $("#days").text(days);
+			  $("#hours").text(hours);
+			  $("#minutes").text(minutes);
+			  $("#seconds").text(seconds);
+			  
+
+			  // If the count down is finished, write some text 
+			  if (distance < 0) {
+				clearInterval(x);
+				document.getElementById("demo").innerHTML = "We are Here...";
+			  }
+			}, 1000);
+};
